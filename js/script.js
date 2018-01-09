abf97a5f83acb2b7fe24cd9b6ea3bd55c457c9b9
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-09 12:51:29 +0530

    Correct fetched link!

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
  * JS
  */
 
-const postsPromise = fetch('http://wesbos.com/wp-jsan/wp/v2/posts');
+const postsPromise = fetch('http://wesbos.com/wp-json/wp/v2/posts');
 postsPromise
   .then(data => data.json())
   .then(data => console.log(data))
