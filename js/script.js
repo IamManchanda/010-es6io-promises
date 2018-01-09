068b52857d2d4078455f7edd5b176fb5ac8fee3a
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-09 13:17:54 +0530

    Building my own Promises

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@
  * JS
  */
 
-const postsPromise = fetch('http://wesbos.com/wp-json/wp/v2/posts');
-postsPromise
-  .then(data => data.json())
-  .then(data => console.log(data))
-  .catch(err => console.error(err));
+const para = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(Error(`Error! Harry isn't cool`));
+  }, 1000);
+  
+});
+
+para
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
