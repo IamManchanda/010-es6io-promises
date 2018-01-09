1df1c87f2f6aa998370270e957bdd6279e134f88
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-09 15:01:41 +0530

    Working with multiple promises!

--- js/script.js
+++ js/script.js
@@ -2,48 +2,33 @@
  * JS
  */
 
-const posts = [
-  { title: 'I love JavaScript', author: 'Wes Bos', id: 1 },
-  { title: 'CSS!', author: 'Chris Coyier', id: 2 },
-  { title: 'Dev tools tricks', author: 'Addy Osmani', id: 3 },
-];
+/*const weather = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve({ temp: 29, conditions: 'Sunny with Clouds'});
+  }, 2000);
+});
 
-const authors = [
-  { name: 'Wes Bos', twitter: '@wesbos', bio: 'Canadian Developer' },
-  { name: 'Chris Coyier', twitter: '@chriscoyier', bio: 'CSS Tricks and CodePen' },
-  { name: 'Addy Osmani', twitter: '@addyosmani', bio: 'Googler' },
-];
+const tweets = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(['I like cake', 'BBQ is good too!']);
+  }, 500);
+});
 
-function getPostById(id) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const post = posts.find(post => post.id === id);
-      if (post) resolve(post);
-      else reject(Error('No Post was found.'));
-    }, 2000)
-  });
-}
+Promise
+  .all([weather, tweets])
+  .then(responses => {
+    const [weatherInfo, tweetsInfo] = responses;
+    console.log(weatherInfo, tweetsInfo);
+  });*/
 
-function hydrateAuthors(post) {
-  return new Promise((resolve, reject) => {
-    const authorDetails = authors.find(person => person.name === post.author);
-    if (authorDetails) {
-      post.author = authorDetails;
-      resolve(post);
-    } else {
-      reject(Error('Can not find the author'));
-    }
-  });
-}
+const postsPromise = fetch('http://wesbos.com/wp-json/wp/v2/posts');
+const streetCarsPromise = fetch('http://data.ratp.fr/api/datasets/1.0/search/?q=paris');
 
-getPostById(1)
-  .then(post => {
-    console.log(post);
-    return hydrateAuthors(post);
+Promise
+  .all([postsPromise, streetCarsPromise])
+  .then(responses => {
+    return Promise.all(responses.map((response) => response.json()));
   })
-  .then(post => {
-    console.log(post);
-  })
-  .catch(error => {
-    console.error(error);
+  .then(responses => {
+    console.log(responses);
   });
